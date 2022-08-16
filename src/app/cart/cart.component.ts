dce740de12addb268908e08edfe625f785f943ad
laxmi narayana <laxminarayanakota8@gmail.com>
2022-08-16 09:20:59 +0530

    data

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  carts:any = [];
+  @Input()  carts:any[] = [];
 
   constructor() { }
 
